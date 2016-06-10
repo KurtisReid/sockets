990ce928261bb5be87c251498822d593df97767e
Kurtis Reid <kreid8@kent.edu>
2016-06-10 16:45:05 -0400

    init commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+run $ node node_server.js
+Then run android application.
+
+Code sources:
+
+Node_server.js
+http://helloraspberrypi.blogspot.com/2014/03/create-tcp-server-using-nodejs-with-net.html
---
+++ node-server/node_server.js
@@ -0,0 +1,47 @@
+var os=require('os');
+var net=require('net');
+
+var networkInterfaces=os.networkInterfaces();
+
+var port = 8081;
+var count = 1;
+
+function callback_server_connection(socket){
+    var remoteAddress = socket.remoteAddress;
+    var remotePort = socket.remotePort;
+    socket.setNoDelay(true);
+    console.log("connected: ", remoteAddress, " : ", remotePort);
+    
+    var msg = 'Hello ' + remoteAddress + ' : ' +  remotePort + '\r\n'
+        + "You are #" + count + '\r\n';
+    count++;
+
+    socket.end(msg);
+    
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+    
+    socket.on('end', function () {
+        console.log("ended: ", remoteAddress, " : ", remotePort);
+    });
+}
+
+console.log("http://android-er.blogspot.com/");
+console.log("http://helloraspberrypi.blogspot.com/");
+
+console.log("node.js net server is waiting:");
+for (var interface in networkInterfaces) {
+
+    networkInterfaces[interface].forEach(function(details){
+        
+        if ((details.family=='IPv4') && !details.internal) {
+            console.log(interface, details.address);  
+        }
+    });
+}
+
+console.log("port: ", port);
+
+var netServer = net.createServer(callback_server_connection);
+netServer.listen(port);
